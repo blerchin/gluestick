40111471273f925f4bb7dce1a71f76c9e93b15dc
Ben Lerchin <ben@lerchin.com>
2012-05-16 18:10:52 +0100

    fixed bug in anchor behavior

--- public/interactions.js
+++ public/interactions.js
@@ -4,7 +4,7 @@ var setToolTip = function(toolState) {
     $('#toolbar ul li.tooltip').text(hint(toolState));
     ///clear all eventhandlers in this context
 	node = svg.selectAll('svg.node').on("click", null).on("mousedown", null).on("mouseup",null) ;
-	
+	if(typeof demoLine != 'undefined'){ demoLine.remove() };
 	switch(toolState) {
 		case "addNode":
 				node.on("click", function(e){ 
@@ -42,15 +42,23 @@ var setToolTip = function(toolState) {
 							.attr('y1', e.y)
 							.attr('x2', e.x)
 							.attr('y2', e.y);	
-							
-						$(document).mousemove(function(e){
+						var execs = 0;
+						svg.on("mousemove",function(e){
+							//e.stopPropagation();
 							demoLine
 								.attr('x1', newSource.x)
 								.attr('y1', newSource.y)
-								.attr('x2', e.offsetX)
-								.attr('y2', e.offsetY);
-						 });
-						 $(document).on("mouseleave", function(e) {
+								.attr('x2', d3.event.offsetX)
+								.attr('y2', d3.event.offsetY);
+							
+							if(execs<20) {
+								console.log(d3.event);
+								}
+							execs++
+							return false;
+						 },false);
+						 
+						 svg.on("mouseleave", function(e) {
 							 demoLine.remove();
 							 return;
 						 	});
@@ -88,7 +96,7 @@ var setToolTip = function(toolState) {
 					e.fixed = 0;
 					});
 				node.on("mouseout", function(e){
-					var url = '/post/id/'+e.id+'/fixed/'+ (e.fixed ? e.fixed + '/'+e.x+'/'+e.y : '/');
+					var url = '/post/id/'+e.id+'/fixed/'+ (e.fixed ? e.fixed + '/'+e.x+'/'+e.y : '/false');
 					$.getJSON(url, function(data) {
 						console.log(data);
 						});
@@ -98,6 +106,34 @@ var setToolTip = function(toolState) {
 			case 'refresh':
 				updateGraph();
 			break;
-						
+			case 'textEdit':
+				node.on("click", function(e) {
+					force.stop();
+					$('#chart').append('<div class = "editBox">\
+												<textarea id="name">'+e.name+'</textarea>\
+												<input id="href" val=("'+( e.href ? e.href.toString() : 'url')+'") />\
+												<button id="editSave" onclick="saveData('+e.id+')" val="save">Save</button>\
+												</div>');
+					var editBox = $('#chart div.editBox');
+					editBox.css({"left": e.x, "top":e.y});
+					
+					
+						});
+			break;						
 		}
 	}
+
+function saveData(nodeId){
+						var editBox = $('#chart div.editBox');
+						$.ajax({
+							url: '/post/id/'+nodeId+'/update/',
+							dataType: 'json',
+							data: {"name": editBox.find('textarea#name').val(),
+								   "href": editBox.find('input#href').val()},
+							success: function() {
+								console.log('data saved!');
+								editBox.remove();
+								updateGraph();
+								}
+							});
+						}
